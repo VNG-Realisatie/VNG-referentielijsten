556396548c4165016b6f6bb6ffdd1206ac122902
Steven Bal <steven@maykinmedia.nl>
2020-08-07 11:18:09 +0200

    Added ProcesType.jaar filter + test

--- src/vrl/selectielijst/api/filters.py
+++ src/vrl/selectielijst/api/filters.py
@@ -1,9 +1,15 @@
 from vng_api_common.filtersets import FilterSet
 
-from ..models import Resultaat
+from ..models import ProcesType, Resultaat
 
 
 class ResultaatFilter(FilterSet):
     class Meta:
         model = Resultaat
         fields = ("proces_type",)
+
+
+class ProcesTypeFilter(FilterSet):
+    class Meta:
+        model = ProcesType
+        fields = ("jaar",)
